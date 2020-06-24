9ef75d34fedaed9ee3b4af819c297b1db7aa04dc
Amit Rawat <beingamit4197@gmail.com>
2020-06-24 12:28:09 +0530

    change in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'https://rails-devise-template.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
