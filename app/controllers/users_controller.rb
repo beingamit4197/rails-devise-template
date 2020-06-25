108b01ca6119833afe78e41e2ada456e1ae62261
Amit Rawat <beingamit4197@gmail.com>
2020-06-25 14:13:30 +0530

    modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
-def create
-        @user = User.new(user_params)    
-      if @user.save
-        UserMailer.registration_confirmation(@user).deliver
-        flash[:success] = "Please confirm your email address to continue"
-        redirect_to root_url
-      else
-        flash[:error] = "Ooooppss, something went wrong!"
-        render 'new'
-      end
+class UsersController < ApplicationController
+  def create
+      @user = User.new(user_params)    
+    if @user.save
+      UserMailer.registration_confirmation(@user).deliver
+      flash[:success] = "Please confirm your email address to continue"
+      redirect_to root_url
+    else
+      flash[:error] = "Ooooppss, something went wrong!"
+      render 'new'
+    end
   end
+end
